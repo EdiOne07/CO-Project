395f89346449a8a4a570bf9a7bcd77efb7f57872
Larisa Jurj <larisajurj21@gmail.com>
2023-04-12 14:39:24 +0300

    Added SceneBuilder and CSS

---
+++ src/Frontend/Controller.java
@@ -0,0 +1,15 @@
+package Frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+
+public class Controller {
+    @FXML
+    public void goToHDDScene(ActionEvent e){
+        System.out.println("hellooooo");
+    }
+    public void goToCPUScene(ActionEvent e){
+        System.out.println("bye");
+
+    }
+}
--- src/Frontend/Main.java
+++ src/Frontend/Main.java
@@ -1,16 +1,13 @@
 package Frontend;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class Main extends Application{
-    Button HDDbutton, CPUbutton, backButton,backButton2;
     Stage window;
-    Scene startingScene, CPUscene, HDDscene;
+    Scene startingScene;
     public static void main(String[] args) {
         launch(args);
     }
@@ -18,53 +15,18 @@ public class Main extends Application{
     @Override
     public void start(Stage primaryStage) throws Exception {
         //here will be the main javafx code
-        window = primaryStage;
-        primaryStage.setTitle("The Mighty Capybara's Benchmark");
-        Image icon = new Image("Frontend/Images/brown.png");
-        window.getIcons().add(icon);
-
-        backButton = new Button();
-        backButton.setText("Click to go back to menu!");
-        backButton.setOnAction( e-> {
+        try {
+            window = primaryStage;
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/Main.fxml"));
+            startingScene = new Scene(layout);
+            //startingScene.getStylesheets().add(getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm());
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            startingScene.getStylesheets().add(css);
             window.setScene(startingScene);
-        });
-
-        backButton2 = new Button();
-        backButton2.setText("Click to go back to menu!");
-        backButton2.setOnAction( e-> {
-            window.setScene(startingScene);
-        });
-
-        CPUbutton = new Button();
-        CPUbutton.setText("Click to test CPU!");
-        CPUbutton.setOnAction( e-> {
-            window.setScene(CPUscene);
-        });
-        //creating layout for CPU test
-        Label label2 = new Label("wow such cool CPU");
-        VBox CPUTestLayout = new VBox();
-        CPUTestLayout.getChildren().addAll(label2, backButton);
-        CPUscene = new Scene(CPUTestLayout,300,200);
-
-
-        HDDbutton = new Button();
-        HDDbutton.setText("Click to test HDD!");
-        HDDbutton.setOnAction( e-> {
-            window.setScene(HDDscene);
-        });
-        //creating layout for HDD test
-        Label label = new Label("wow such cool HDD");
-        VBox HDDTestLayout = new VBox();
-        HDDTestLayout.getChildren().addAll(label, backButton2);
-        HDDscene = new Scene(HDDTestLayout,300,200);
-        //creating the layout of the  starting scene
-        VBox layout = new VBox();
-        layout.getChildren().addAll(HDDbutton,CPUbutton);
-
-        startingScene = new Scene(layout, 300, 200);
-        window.setScene(startingScene);
-        window.show();
-
+            window.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 
 }
