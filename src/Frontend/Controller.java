4dc62d4d50a87baa135f35aa84af1aef8234f58b
Larisa Jurj <larisajurj21@gmail.com>
2023-04-12 15:49:17 +0300

    Added HDD and CPU scenes + background

--- src/Frontend/Controller.java
+++ src/Frontend/Controller.java
@@ -2,14 +2,44 @@ package Frontend;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class Controller {
     @FXML
-    public void goToHDDScene(ActionEvent e){
-        System.out.println("hellooooo");
+    private Stage stage;
+    private Scene scene;
+    private Parent layout;
+    public void goToHDDScene(ActionEvent event) throws IOException {
+        try{
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/HDDScene.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(layout);
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
-    public void goToCPUScene(ActionEvent e){
-        System.out.println("bye");
+    public void goToCPUScene(ActionEvent event) throws IOException{
+        try{
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/CPUScene.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(layout);
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
 
-    }
 }
---
+++ src/Frontend/HDDSceneController.java
@@ -0,0 +1,29 @@
+package Frontend;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HDDSceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent layout;
+    public void goBack(ActionEvent event) throws IOException {
+        try{
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/Main.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(layout);
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+}
