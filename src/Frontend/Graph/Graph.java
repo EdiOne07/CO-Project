8d293089d1b3294d6e1b8da74abcaa3d973247a6
GIoana02 <ioana.grigoriu@student.upt.ro>
2023-05-04 12:53:19 +0300

    Test Graph - Working

--- src/Frontend/Graph/Graph.java
+++ src/Frontend/Graph/Graph.java
@@ -1,55 +1,60 @@
 package Frontend.Graph;
-import com.sun.javafx.geom.AreaOp;
-import javafx.scene.chart.Chart;
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.chart.BarChart;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.NumberAxis;
 import javafx.scene.chart.XYChart;
-import javafx.scene.control.TableColumn;
 import javafx.stage.Stage;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.sql.SQLOutput;
+import java.io.IOException;
 
-public class Graph {
+public class Graph extends Application {
 
-    String line="";
-    String buffer="";
-    BufferedReader sc;
-    String splitBy=";";
-    String[] computer_data;
-    public void scan () throws Exception {
-        //parsing a CSV file into Scanner class constructor
-        sc = new BufferedReader(new FileReader("D:\\Faculty\\CO-Project\\Test2.csv"));
-        while ((line=sc.readLine())!= null)
-        {
-            computer_data =line.split(splitBy);
-            System.out.println("Computer: " + computer_data[0] +", CPU model: " + computer_data[1] + ", Score: "+ computer_data[2]);  //find and returns the next complete token from this scanner
+    public void start(Stage stage) {
+        String csvFile = "src\\Frontend\\Graph\\Test3.csv";
+        String line = "";
+        String csvSplitBy = ";";
+
+        // Create a new XYChart series for the data
+        XYChart.Series<Number, String> series = new XYChart.Series<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
+            while ((line = br.readLine()) != null) {
+                // Split the CSV row into an array of values
+                String[] data = line.split(csvSplitBy);
+
+                // Add a new data point to the series for each row in the CSV
+                series.getData().add(new XYChart.Data<>(Double.parseDouble(data[2]), data[1]));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
-        sc.close();  //closes the scanner
+
+        // Create the x and y axes
+        NumberAxis xAxis = new NumberAxis();
+        CategoryAxis yAxis = new CategoryAxis();
+
+        // Create the bar chart
+        BarChart<Number, String> barChart = new BarChart<>(xAxis, yAxis);
+        barChart.setTitle("Computer Performance Scores");
+        barChart.setLegendVisible(false);
+
+        // Add the data series to the chart
+        barChart.getData().add(series);
+
+        // Create a new scene and add the chart to it
+        Scene scene = new Scene(barChart, 800, 600);
+
+        // Set the scene and show the stage
+        stage.setScene(scene);
+        stage.show();
     }
 
-    @Override public void start(Stage stage) {
-        stage.setTitle("Test Chart Sample");
-        final CategoryAxis xAxis = new CategoryAxis();
-        final NumberAxis yAxis = new NumberAxis();
-        final BarChart<String, Number> bc = new BarChart<String, Number>(xAxis, yAxis);
-        bc.setTitle("Computer Score Test");
-        xAxis.setLabel("Score");
-        yAxis.setLabel("Computer");
-        Graph n1=new Graph();
-        XYChart.Series series1 = new XYChart.Series();
-        series1.setName("Computer1");
-        while ((buffer=n1.scan())!=null) {
-            series1.getData().add(new XYChart.Data(computer_data[1], computer_data[2]));
-            series1.getData().add(new XYChart.Data(computer_data[1], computer_data[2]));
-            series1.getData().add(new XYChart.Data(computer_data[1], computer_data[2]));
-            series1.getData().add(new XYChart.Data(computer_data[1], computer_data[2]));
-            series1.getData().add(new XYChart.Data(computer_data[1], computer_data[2]));
-        }
+    public static void main(String[] args) {
+        launch(args);
     }
-
 }
