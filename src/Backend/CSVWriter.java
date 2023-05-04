821fc7b28a4cd310d7c4983c866f61f303aa8689
EdiOne07 <edione2002@gmail.com>
2023-05-04 15:36:51 +0300

    Added Database button and functionalities

--- src/Backend/CSVWriter.java
+++ src/Backend/CSVWriter.java
@@ -1,9 +1,10 @@
 package Backend;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CSVWriter {
-    public static void main(String[] args){
+    public void CreateCSV(){
         String csvfile="test.csv";
         FileWriter writer=null;
         try{
@@ -13,11 +14,20 @@ public class CSVWriter {
                     writer.append("Hazt");
                     writer.append(",");
                     writer.append("John");
+                    writer.append(",");
+                    writer.append("Chel");
+                    writer.append(",");
+                    writer.append("1");
+                    writer.append(",");
             }
             writer.append("\n");
             for(int i=0;i<10;i++){
                 writer.append("Dorian");
                 writer.append(",");
+                writer.append("John");
+                writer.append(",");
+                writer.append("Chel");
+                writer.append((char) i);
             }
         } catch (IOException e) {
             throw new RuntimeException(e);
@@ -30,5 +40,35 @@ public class CSVWriter {
                System.out.println("Couldn't close");
             }
         }
+    }
+    public List<Elements> readCSV(String filename) {
+        BufferedReader reader = null;
+        List<Elements> info=new ArrayList<>();
+        try {
+            reader = new BufferedReader(new FileReader(filename));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                String[] parts = line.split(",");
+                String cpu = parts[0];
+                String hdd = parts[1];
+                String pc=parts[2];
+                int score= Integer.parseInt(parts[3]);
+                System.out.println("CPU : " + cpu);
+                System.out.println("Hdd : " + hdd);
+                Elements element=new Elements(cpu,hdd,pc,score);
+                info.add(element);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } finally {
+            try {
+                if (reader != null) {
+                    reader.close();
+                }
+            } catch (IOException e) {
+                System.out.println("Couldn't close");
+            }
+        }
+        return info;
     }
 }
--- src/Frontend/Controller.java
+++ src/Frontend/Controller.java
@@ -1,20 +1,31 @@
 package Frontend;
 
+import Backend.CSVWriter;
+import Backend.Elements;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.util.List;
 
 public class Controller {
     @FXML
     private Stage stage;
     private Scene scene;
     private Parent layout;
+    private TableView<Elements> tableView;
+    private TableColumn<Elements,String> computer_Name;
+    private TableColumn<Elements,String> cpu;
+    private TableColumn<Elements,String> hdd;
+    private TableColumn<Elements,Integer> score;
     public void goToHDDScene(ActionEvent event) throws IOException {
         try{
             Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/HDDScene.fxml"));
@@ -40,6 +51,20 @@ public class Controller {
         }catch(Exception e){
             e.printStackTrace();
         }
+    }
+    public void CSV(ActionEvent event) throws IOException{
+        try{
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/DatabaseScene.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(layout);
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+
     }
 
 }
