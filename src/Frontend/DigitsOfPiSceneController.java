6c8aeecff944ed5881a501becf978b20cefdf0bd
Larisa Jurj <larisajurj21@gmail.com>
2023-05-14 18:16:24 +0300

    added background for benchmark scene

--- src/Frontend/DigitsOfPiSceneController.java
+++ src/Frontend/DigitsOfPiSceneController.java
@@ -2,11 +2,21 @@ package Frontend;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Slider;
 import Frontend.TestBenchmark.TestDigitsOfPi;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class DigitsOfPiSceneController {
     private  TestDigitsOfPi test;
+    private Stage stage;
+    private Scene scene;
+    private Parent layout;
     @FXML
     private Slider slider;
     public DigitsOfPiSceneController(){
@@ -18,5 +28,18 @@ public class DigitsOfPiSceneController {
         Integer load = (int) (slider.getValue());
         test.run(load);
         System.out.println("Finished in " + test.getTime() + " s");
+    }
+    public void goBack(ActionEvent event) throws IOException {
+        try{
+            Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/CPUScene.fxml"));
+            stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+            scene = new Scene(layout);
+            String css = this.getClass().getClassLoader().getResource("Frontend/Style.css").toExternalForm();
+            scene.getStylesheets().add(css);
+            stage.setScene(scene);
+            stage.show();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
     }
 }
