f154e298feb6c6f0a301a1d99ffe420652e6d26b
Eric Florea <eric.florea@student.upt.ro>
2023-05-24 22:30:06 +0300

    "Added SystemInfo class for CSV"

--- src/Frontend/SystemInfoController.java
+++ src/Frontend/SystemInfoController.java
@@ -1,5 +1,6 @@
 package Frontend;
 
+import Backend.SystemInfo;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -36,79 +37,13 @@ public class SystemInfoController {
     @FXML
     public void initialize() {
         OperatingSystemMXBean osBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        RuntimeMXBean processorname = ManagementFactory.getRuntimeMXBean();
-        Runtime memory = Runtime.getRuntime();
-        OperatingSystemMXBean operatingSystemBean = (OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean();
-        long ram = ((com.sun.management.OperatingSystemMXBean) ManagementFactory.getOperatingSystemMXBean()).getTotalMemorySize();
-
-        String userName = System.getProperty("user.name");
-
-        StringBuilder hdd = new StringBuilder();
-        File[] roots = File.listRoots();
-        hdd.append("HDDs/SSDs:\n");
-        for (File root : roots) {
-            long totalSpace = root.getTotalSpace();
-            long freeSpace = root.getFreeSpace();
-            hdd.append(root.getPath()).append(" - Total Space: ").append(totalSpace / (1024 * 1024 * 1024)).append(" GB\nFree Space: ").append(freeSpace / (1024 * 1024 * 1024)).append(" \n");
-        }
-
+        SystemInfo getInfo = new SystemInfo();
 
         OSLabel.setText("OS:" + osBean.getName() + " "+ osBean.getVersion());
-        CPULabel.setText("CPU: " + getCpuModel());
-        RAMLabel.setText("RAM: " + (ram / (1024 * 1024)) + " MB");
-        UserLabel.setText("Username: " + userName);
-        HDDLabel.setText(hdd.toString());
-
-    }
-
-    public String getCpuModel() {
-        String osName = System.getProperty("os.name").toLowerCase();
-        String arch = System.getProperty("os.arch").toLowerCase();
-        String cpuModelWindows = "Unknown";
-
-        if (osName.contains("win")) { // Windows
-            try {
-                Process process = Runtime.getRuntime().exec("wmic cpu get name");
-                Scanner scanner = new Scanner(process.getInputStream());
-                while (scanner.hasNextLine()) {
-                    String line = scanner.nextLine().trim();
-                    if (!line.isEmpty() && !line.equalsIgnoreCase("name")) {
-                        cpuModelWindows = line;
-                        break;
-                    }
-                }
-                scanner.close();
-                return  cpuModelWindows.trim();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        } else if (osName.contains("mac")) { // macOS
-            try {
-                Process process = Runtime.getRuntime().exec("sysctl -n machdep.cpu.brand_string");
-                Scanner scanner = new Scanner(process.getInputStream());
-                String cpuModel = scanner.nextLine();
-                scanner.close();
-                return cpuModel.trim();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Unknown";
-            }
-        } else if (osName.contains("nix") || osName.contains("nux") || osName.contains("aix")) { // Linux/Unix
-            try {
-                Process process = Runtime.getRuntime().exec("cat /proc/cpuinfo | grep 'model name' | uniq");
-                Scanner scanner = new Scanner(process.getInputStream());
-                String cpuModel = scanner.nextLine();
-                scanner.close();
-                return cpuModel.substring(cpuModel.indexOf(':') + 1).trim();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return "Unknown";
-            }
-        } else { // Unsupported OS
-            return "Unknown";
-        }
-
-        return "Unknown";
+        CPULabel.setText("CPU: " + getInfo.getCpuModel());
+        RAMLabel.setText("RAM: " + (getInfo.getRAM() / (1024 * 1024)) + " MB");
+        UserLabel.setText("Username: " + getInfo.getUserName());
+        HDDLabel.setText(getInfo.getHDD());
     }
 
     public void goBack(ActionEvent event) throws IOException {
