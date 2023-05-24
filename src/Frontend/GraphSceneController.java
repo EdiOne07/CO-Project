ffb5c2df128bee08f50b994195e97d01c4018989
EdiOne07 <edione2002@gmail.com>
2023-05-24 21:48:43 +0300

    Changes by Larisa

--- src/Frontend/GraphSceneController.java
+++ src/Frontend/GraphSceneController.java
@@ -1,11 +1,19 @@
 package Frontend;
 
 import Frontend.Graph.Graph;
+import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Group;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.chart.BarChart;
+import javafx.scene.chart.CategoryAxis;
+import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -13,10 +21,17 @@ import java.net.URL;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
-public class GraphSceneController {
+public class GraphSceneController implements Initializable{
     private Stage stage;
     private Scene scene;
     private Parent layout;
+    @FXML
+    private NumberAxis yAxis;
+    @FXML
+    private CategoryAxis xAxis;
+    @FXML
+    private BarChart<String, Number> XYChart;
+
     public void goBack(ActionEvent event) throws IOException {
         try{
             Parent layout = FXMLLoader.load(getClass().getClassLoader().getResource("Frontend/Main.fxml"));
